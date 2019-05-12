e896da5ce3d6353d2c916fde660154aefb7ff087
Igor Krivenko <igor.s.krivenko@gmail.com>
2019-05-12 19:43:47 -0400

    Added basic CMake structure

---
+++ include/libcommute/version.hpp
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ *
+ * This file is part of libcommute, a C++ 11/14/17 header-only library
+ * to manipulate polynomial expressions with quantum-mechanical operators.
+ *
+ * Copyright (C) 2016-2019 Igor Krivenko <igor.s.krivenko@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ ******************************************************************************/
+#pragma once
+
+#define LIBCOMMUTE_VERSION "0.1"
