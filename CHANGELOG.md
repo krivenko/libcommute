0140f8bf7c1156c8d98e8ce6a56e178203a118c2
Igor Krivenko <igor.s.krivenko@gmail.com>
2021-08-06 10:43:51 +0200

    cmake: Optionally run static analysis tools on unit tests and examples
    Passing `-DSTATIC_ANALYSIS=ON` to CMake will make it run `clang-tidy`
    and `cppcheck` on sources of unit tests and examples.
    
    The installation document has been extended accordingly.
    
    The GitHub Actions workflow will now use the static analysis tools.
    
    Some of `clang-tidy`'s checks have been disaled.
    
    * `modernize-use-trailing-return-type`. I prefer to use the trailing
      return type only when it makes for cleaner code.
    * `cppcoreguidelines-avoid-magic-numbers` is generally unhelpful,
      especially in the unit tests.
    * `cppcoreguidelines-pro-bounds-array-to-pointer-decay` complains about
      every use of `assert()`, see isocpp/CppCoreGuidelines#1589.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+- New CMake option ``STATIC_ANALYSIS``. When enabled, the ``clang-tidy`` and
+  ``cppcheck`` static analysis tools will be run on the C++ sources of unit
+  tests and examples as part of build process.
 - New method ``space_partition.find_connections()``.
 - Added an implementation of the ``StateVector`` concept for some
   [Eigen 3](https://eigen.tuxfamily.org/) types (vectors, vector segments,
---
+++ cmake/StaticAnalysis.cmake
@@ -0,0 +1,46 @@
+#
+# This file is part of libcommute, a quantum operator algebra DSL and
+# exact diagonalization toolkit for C++11/14/17.
+#
+# Copyright (C) 2016-2021 Igor Krivenko <igor.s.krivenko@gmail.com>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#
+# clang-tidy
+#
+
+find_program(CLANG_TIDY_EXECUTABLE
+             NAMES "clang-tidy" REQUIRED
+             DOC "Path to the clang-tidy executable")
+mark_as_advanced(CLANG_TIDY_EXECUTABLE)
+
+message(STATUS "Using clang-tidy: ${CLANG_TIDY_EXECUTABLE}")
+set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_EXECUTABLE}")
+
+#
+# cppcheck
+#
+
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.10)
+  find_program(CPPCHECK_EXECUTABLE
+               NAMES "cppcheck" REQUIRED
+               DOC "Path to the cppcheck executable")
+  mark_as_advanced(CPPCHECK_EXECUTABLE)
+
+  message(STATUS "Using cppcheck: ${CPPCHECK_EXECUTABLE}")
+  set(CMAKE_CXX_CPPCHECK
+      "${CPPCHECK_EXECUTABLE}"
+      "--enable=warning,style,performance,portability"
+      "--std=c++17"
+      "--template=gcc"
+      "--inline-suppr"
+      "--verbose"
+      "--force"
+      "--quiet"
+  )
+else(CMAKE_VERSION VERSION_GREATER_EQUAL 3.10)
+  message(WARNING "CMake >= 3.10 is required to run cppcheck")
+endif(CMAKE_VERSION VERSION_GREATER_EQUAL 3.10)
