de2ee7a5f0597f66fcbd3d53bef633a86725cde5
Igor Krivenko <iskrivenko@proton.me>
2024-11-10 12:14:39 +0100

    Remove e-mail address from MPL headers

--- cmake/FindSphinx.cmake
+++ cmake/FindSphinx.cmake
@@ -2,7 +2,7 @@
 # This file is part of libcommute, a quantum operator algebra DSL and
 # exact diagonalization toolkit for C++11/14/17.
 #
-# Copyright (C) 2016-2024 Igor Krivenko <igor.s.krivenko@gmail.com>
+# Copyright (C) 2016-2024 Igor Krivenko
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
