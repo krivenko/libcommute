184a469fbcefc506bc31c09beb17402e503d1651
Igor Krivenko <iskrivenko@proton.me>
2025-01-02 12:19:57 +0100

    Update copyright years

--- test/generator.cpp
+++ test/generator.cpp
@@ -3,7 +3,7 @@
  * This file is part of libcommute, a quantum operator algebra DSL and
  * exact diagonalization toolkit for C++11/14/17.
  *
- * Copyright (C) 2016-2024 Igor Krivenko
+ * Copyright (C) 2016-2025 Igor Krivenko
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
