863fde23817e8d1ba1cf742ead46c75ad5846c8e
Igor Krivenko <iskrivenko@proton.me>
2025-09-22 11:45:03 +0200

    test: Extend commutator tests to ScalarType = int

--- test/commutators.cpp
+++ test/commutators.cpp
@@ -18,23 +18,22 @@
 
 #include <libcommute/expression/expression.hpp>
 #include <libcommute/expression/factories.hpp>
-#include <libcommute/expression/generator_fermion.hpp>
 
 using namespace libcommute;
 
-template <typename ScalarType> void make_commutators_test_case() {
+template <typename ExprT> ExprT comm(ExprT const& expr1, ExprT const& expr2) {
+  return expr1 * expr2 - expr2 * expr1;
+}
+template <typename ExprT> ExprT acomm(ExprT const& expr1, ExprT const& expr2) {
+  return expr1 * expr2 + expr2 * expr1;
+}
+
+template <typename ScalarType> void make_commutators_test_case_fermions() {
 
   using namespace static_indices;
 
   using S = ScalarType;
   using expr_t = expression<ScalarType, int>;
-
-  auto comm = [](expr_t const& expr1, expr_t const& expr2) {
-    return expr1 * expr2 - expr2 * expr1;
-  };
-  auto acomm = [](expr_t const& expr1, expr_t const& expr2) {
-    return expr1 * expr2 + expr2 * expr1;
-  };
 
   SECTION("Fermions") {
     int const N = 4;
@@ -52,6 +51,14 @@ template <typename ScalarType> void make_commutators_test_case() {
       }
     }
   }
+}
+
+template <typename ScalarType> void make_commutators_test_case_bosons() {
+
+  using namespace static_indices;
+
+  using S = ScalarType;
+  using expr_t = expression<ScalarType, int>;
 
   SECTION("Bosons") {
     int const N = 4;
@@ -69,6 +76,13 @@ template <typename ScalarType> void make_commutators_test_case() {
       }
     }
   }
+}
+
+template <typename ScalarType> void make_commutators_test_case_spins() {
+
+  using namespace static_indices;
+
+  using S = ScalarType;
 
   SECTION("Spin") {
     int const N = 4;
@@ -77,46 +91,57 @@ template <typename ScalarType> void make_commutators_test_case() {
       for(int j = 0; j < N; ++j) {
         // Spin-1/2
         auto comm_Sp_Sm = comm(S_p<S>(i), S_m<S>(j));
-        CHECK(comm_Sp_Sm == 2.0 * S(i == j) * S_z<S>(i));
+        CHECK(comm_Sp_Sm == S(2 * (i == j)) * S_z<S>(i));
 
         auto comm_Sz_Sp = comm(S_z<S>(i), S_p<S>(j));
         CHECK(comm_Sz_Sp == S(i == j) * S_p<S>(i));
 
         auto comm_Sz_Sm = comm(S_z<S>(i), S_m<S>(j));
-        CHECK(comm_Sz_Sm == -1.0 * S(i == j) * S_m<S>(i));
+        CHECK(comm_Sz_Sm == S(-(i == j)) * S_m<S>(i));
 
         // Spin-1
         auto comm_S1p_S1m = comm(S_p<3, S>(i), S_m<3, S>(j));
-        CHECK(comm_S1p_S1m == 2.0 * S(i == j) * S_z<3, S>(i));
+        CHECK(comm_S1p_S1m == S(2 * (i == j)) * S_z<3, S>(i));
 
         auto comm_S1z_S1p = comm(S_z<3, S>(i), S_p<3, S>(j));
         CHECK(comm_S1z_S1p == S(i == j) * S_p<3, S>(i));
 
         auto comm_S1z_S1m = comm(S_z<3, S>(i), S_m<3, S>(j));
-        CHECK(comm_S1z_S1m == -1.0 * S(i == j) * S_m<3, S>(i));
+        CHECK(comm_S1z_S1m == S(-(i == j)) * S_m<3, S>(i));
 
         // Spin-3/2
         auto comm_S32p_S32m = comm(S_p<4, S>(i), S_m<4, S>(j));
-        CHECK(comm_S32p_S32m == 2.0 * S(i == j) * S_z<4, S>(i));
+        CHECK(comm_S32p_S32m == S(2 * (i == j)) * S_z<4, S>(i));
 
         auto comm_S32z_S32p = comm(S_z<4, S>(i), S_p<4, S>(j));
         CHECK(comm_S32z_S32p == S(i == j) * S_p<4, S>(i));
 
         auto comm_S32z_S32m = comm(S_z<4, S>(i), S_m<4, S>(j));
-        CHECK(comm_S32z_S32m == -1.0 * S(i == j) * S_m<4, S>(i));
+        CHECK(comm_S32z_S32m == S(-(i == j)) * S_m<4, S>(i));
       }
     }
   }
 }
 
+TEST_CASE("Commutation relations (int)", "[commutators_int]") {
+  make_commutators_test_case_fermions<int>();
+  make_commutators_test_case_bosons<int>();
+}
+
 TEST_CASE("Commutation relations (double)", "[commutators_double]") {
-  make_commutators_test_case<double>();
+  make_commutators_test_case_fermions<double>();
+  make_commutators_test_case_bosons<double>();
+  make_commutators_test_case_spins<double>();
 }
 
 TEST_CASE("Commutation relations (complex)", "[commutators_complex]") {
-  make_commutators_test_case<std::complex<double>>();
+  make_commutators_test_case_fermions<std::complex<double>>();
+  make_commutators_test_case_bosons<std::complex<double>>();
+  make_commutators_test_case_spins<std::complex<double>>();
 }
 
 TEST_CASE("Commutation relations (my_complex)", "[commutators_my_complex]") {
-  make_commutators_test_case<my_complex>();
+  make_commutators_test_case_fermions<my_complex>();
+  make_commutators_test_case_bosons<my_complex>();
+  make_commutators_test_case_spins<my_complex>();
 }
